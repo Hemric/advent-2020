81de34871874d6bb81737d9ab39264670693ecfa
Henry Dufaure de Citres <hddc.work@gmail.com>
2020-12-03 18:50:31 +0100

    Day_3 2/2

--- src/main.rs
+++ src/main.rs
@@ -80,12 +80,27 @@ fn day_3() {
     let data = load_data("./data/day_3.txt");
     let lines: Vec<&str> = data.split("\n").collect();
 
+    let configs = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
+
+    let mut answer: u32 = 1;
+
+    for config in configs.iter() {
+        answer *= slop(&lines, config[0], config[1]);
+    }
+
+    println!("Answer : {}", answer);
+}
+
+fn slop(lines: &Vec<&str>, right: usize, down: usize) -> u32 {
     let mut tree_count = 0;
-    let mut line_index = 1; // first line is ignored
-    let mut index = 3;
+    let mut line_index = down; // first line is ignored
+    let mut index = right;
 
     loop {
-        let line = lines[line_index];
+        let line = match lines.get(line_index) {
+            Some(x) => x,
+            None => break,
+        };
 
         let spot = match line.chars().nth(index) {
             Some(x) => x,
@@ -103,9 +118,11 @@ fn day_3() {
             tree_count += 1;
         }
 
-        index += 3;
-        line_index += 1;
+        index += right;
+        line_index += down;
     }
 
-    println!("Answer : {}", tree_count);
+    println!("Right {}, Down {}, Tree {}", right, down, tree_count);
+
+    tree_count
 }
