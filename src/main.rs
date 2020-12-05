2ab4b0c3024574683ffea6d6ee52ea844693d5ff
Henry Dufaure de Citres <hddc.work@gmail.com>
2020-12-05 15:35:35 +0100

    Day_5 2/2

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ fn main() {
         2 => day_2(),
         3 => day_3(),
         4 => day_4(),
+        5 => day_5(),
         _ => println!("Challenge not found"),
     }
 }
@@ -32,7 +33,7 @@ fn load_data(path: &str) -> String {
 
     file.read_to_string(&mut data).expect("Error reading file");
 
-    data
+    data.trim_end().to_string()
 }
 
 fn day_1() {
@@ -168,4 +169,49 @@ fn day_4() {
 
     println!("Answer 1/2: {}", valid_passport_count_1);
     println!("Answer 2/2: {}", valid_passport_count_2);
+}
+
+fn day_5() {
+    let data = load_data("./data/day_5.txt");
+    let tickets: Vec<&str> = data.split("\n").collect();
+    let mut seats: Vec<u32> = [].to_vec();
+
+    for ticket in tickets {
+        let row = search((0.0, 127.0), ticket[..7].chars().rev().collect()) as u32;
+        let column = search((0.0, 7.0), ticket[7..].chars().rev().collect()) as u32;
+        seats.push(row * 8 + column);
+    }
+
+    seats.sort();
+
+    let mut my_seat: u32 = 0;
+
+    for (i, seat) in seats.iter().enumerate() {
+        if seats[i+1] != seat + 1 {
+            my_seat = seat + 1;
+            break;
+        }
+    }
+
+    println!("Answer 1/2: {}", seats.last().unwrap());
+    println!("Answer 2/2: {}", my_seat);
+}
+
+fn search(mut range: (f32, f32), mut letters: Vec<char>) -> f32 {
+    let letter = match letters.pop() {
+        Some(l) => l.to_string(),
+        None => return range.0,
+    };
+
+    let total_range = range.0 + range.1;
+
+    if letter == "F" || letter == "L" {
+        range.1 = (total_range / 2.0).floor();
+    }
+
+    if letter == "B" || letter == "R" {
+        range.0 = (total_range / 2.0).ceil();
+    }
+
+    search(range, letters)
 }
