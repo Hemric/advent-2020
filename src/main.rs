0d9d301c9bd565ec83028cfe0f822097dda34acc
Henry Dufaure de Citres <hddc.work@gmail.com>
2020-12-06 16:22:14 +0100

    Day_6 2/2

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::path::Path;
+use std::collections::HashMap;
 
 fn main() {
     println!("Which day ?");
@@ -20,6 +21,7 @@ fn main() {
         3 => day_3(),
         4 => day_4(),
         5 => day_5(),
+        6 => day_6(),
         _ => println!("Challenge not found"),
     }
 }
@@ -214,4 +216,36 @@ fn search(mut range: (f32, f32), mut letters: Vec<char>) -> f32 {
     }
 
     search(range, letters)
+}
+
+fn day_6() {
+    let data = load_data("./data/day_6.txt");
+    let groups: Vec<&str> = data.split("\n\n").collect();
+
+    let mut sum_1: usize = 0;
+    let mut sum_2: usize = 0;
+
+    for group in groups {
+        let mut map = HashMap::new();
+        for (_i, c) in group.chars()
+            .filter(|c| c.to_string() != "\n")
+            .enumerate() {
+                map.entry(c)
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1);
+        }
+
+        let member_count = group.lines().count();
+
+        sum_1 += map.iter().count();
+        sum_2 += map.iter().fold(0, |mut acc, c| {
+            if c.1 == &member_count {
+                acc += 1;
+            }
+            acc
+        })
+    }
+
+    println!("Answer 1/2: {}", sum_1);
+    println!("Answer 2/2: {}", sum_2);
 }
